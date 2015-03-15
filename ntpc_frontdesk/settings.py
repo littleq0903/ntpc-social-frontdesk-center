64157b484f4830e71b9659f81a4f62bea6cff27c
LittleQ <littleq0903@gmail.com>
2015-03-15 15:28:31 +0800

    bootstrap archi

--- ntpc_frontdesk/settings.py
+++ ntpc_frontdesk/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
 )
 
 BOWER_INSTALLED_APPS = (
+    'bootstrap',
     'bootstrap-material-design',
 )
 
--- ntpc_frontdesk/urls.py
+++ ntpc_frontdesk/urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ntpc_frontdesk.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^app/', include('apps.main.urls')),
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
 )
