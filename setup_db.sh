ecc59b735286b8e54b106b52d83f6db08144bcb3
Colin Su <littleq0903@gmail.com>
2015-06-10 21:52:20 +0800

    mount folder as database and media files

--- setup_db.sh
+++ setup_db.sh
@@ -1,3 +1,2 @@
 python manage.py migrate
-python manage.py collectstatic
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('social', 'social@example.com', 'social26221020')" | python manage.py shell
