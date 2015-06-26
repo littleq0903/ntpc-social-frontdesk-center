a3d1e22d33213bbffef2110441d59fc658cabfbb
LittleQ <littleq0903@gmail.com>
2015-06-26 19:58:09 +0800

    automatically redirect to login page after logged out

--- ntpc_frontdesk/urls.py
+++ ntpc_frontdesk/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^app/', include('apps.main.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^logout/', 'django.contrib.auth.views.logout_then_login'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT,
     }),
